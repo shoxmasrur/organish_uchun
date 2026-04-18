ec90b3b0be74d33d9c4d231b8191bbd24707a2aa
Shoxrux Ibodullayev <shoxmasrur2021@gmail.com>
2026-04-18 13:40:52 +0500

    Create sinfish.py

---
+++ sinfish.py
@@ -0,0 +1,32 @@
+# class salom :
+#     def show(self) :
+#         print("salom")
+# a=salom()
+# a.show()
+
+
+# class Animal():
+#     def __init__(self, a,b):
+#         self.a=a
+#         self.b=b
+
+# class Animal():
+#     def __init__(self, a,b,c,d,g,e,w) :
+#         self.a=a
+#         self.b=b
+#         self.c=c
+#         self.d=d
+#         self.g=g
+#         self.e=e
+#         self.w=w
+#     def kirim(self):
+#             if self.a==self.b:
+#                 print("TOgri")
+#     def chiqim(self, bir, ikki):
+#             if bir>ikki :
+#                 print("bir katta")
+# sabzi=Animal(1,1,3,4,5,6,7)
+# sabzi.kirim()
+# sabzi.chiqim(13,12)
+
+# class car :
